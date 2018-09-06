28f56bdbaa02dbde9762abdca47853e6367a946d
pete <pete.alonzi@gmail.com>
2018-09-06 09:24:25 -0400

    Add files via upload

---
+++ 2018-09-04-python-intro.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  4 14:25:53 2018
+
+@author: lpa2a
+"""
+
+#print('hello world')
+
+
+#print(a)
+
+#fruit = 'apple'
+
+#print(fruit[0])
+#print(fruit[3])
+
+
+fruits = ['apple','banana','cherry']
+#print(fruits[0])
+
+#test = \
+ # ['apple','banana','dog']
+  
+  # add float, int bool to notes
+  
+
+#for x in range(10):
+# comment
+#    print(x)
+#    print(x*10)
+    
+#print('done')
+
+
+#for fruit in fruits:
+#    print(fruit)
+
+#i=0
+
+#while i<10:
+#    print(i)
+#    i+=1
+
+#'apple'  letters
+
+#if 9==10:
+#    print('ten')
+#elif 8==8:
+#    print('eight')
+#else:
+#    print('nine')
+
+
+import numpy
+print(numpy.random.randn())
+
